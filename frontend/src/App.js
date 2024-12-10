ba832e6f764fdb6dd323c4b4a15b0d8ece179587
Peter Cheng <shuoc4@illinois.edu>
2024-12-10 11:04:01 -0600

    Add home and statistics pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import Login from "./components/login/Login"
 import Signup from "./components/login/Signup"
 import Profile from "./components/user/Profile"
 import Home from "./components/home/Home"
+import Statistics from "./components/home/Statistics"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/statistics" element={<Statistics />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
---
+++ frontend/src/components/home/StatusOptions.js
@@ -0,0 +1,24 @@
+const StatusOptions = [
+    {
+        value: "Applied",
+        label: "Applied",
+        color: "#FFB300", // Amber (Dark Yellow)
+    },
+    {
+        value: "Interview Scheduled",
+        label: "Interview Scheduled",
+        color: "#388E3C", // Dark Green
+    },
+    {
+        value: "Rejected",
+        label: "Rejected",
+        color: "#D32F2F", // Dark Red
+    },
+    {
+        value: "Offered",
+        label: "Offered",
+        color: "#0288D1", // Deep Sky Blue
+    }
+]
+
+export default StatusOptions
